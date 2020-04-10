d88f41bdf30329eedefe024d4c529d9cdb551f20
Anderson Begossi <andersonbegossi@gmail.com>
2020-04-09 23:16:04 -0300

    Added schedule list

--- src/pages/_layouts/default/styles.js
+++ src/pages/_layouts/default/styles.js
@@ -1,6 +1,6 @@
 import styled from 'styled-components';
 
 export const Wrapper = styled.div`
-  min-height: 850px;
+  min-height: 950px;
   background: linear-gradient(-90deg, #7159c1, #ab59c1);
 `;
